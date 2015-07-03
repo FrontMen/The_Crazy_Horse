ed6f156fc1e6174459abb33d3678c8e6ea6f0775
Zukzuk <dave.timmerman@gmail.com>
2015-07-03 11:38:02 +0200

    added working sockets

--- gameserver/index.js
+++ gameserver/index.js
@@ -1,17 +1,44 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var people = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+  socket.on("join", function(name){
+    people[socket.id] = name;
+    socket.emit("join", name + " has connected to the server.");
+  });
+
+  socket.on("ready", function(){
+    people[socket.id].ready = true;
+    socket.emit("update", "You have connected to the server.");
+  });
+
+  socket.on('countdown', function(msg){
+    io.emit('countdown', msg);
+  });
+
+  socket.on('end', function(msg){
+    io.emit('countdown', msg);
+  });
+
+  socket.on("send", function(msg){
+    //socket.sockets.emit("chat", people[client.id], msg);
+  });
+
+  socket.on("disconnect", function(){
+    //socket.sockets.emit("update", people[socket.id] + " has left the server.");
+    //delete people[socket.id];
+    //socket.sockets.emit("update-people", people);
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
