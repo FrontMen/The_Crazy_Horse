d4ff71d0d835173b867522993270e3dd961bb45a
Zukzuk <dave.timmerman@gmail.com>
2015-07-03 12:36:04 +0200

    added working countdown

--- gameserver/index.js
+++ gameserver/index.js
@@ -1,44 +1,107 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var people = {};
+var people = Dictionary();
+var socket;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (_socket) {
+  socket = _socket;
 
-  socket.on("join", function(name){
-    people[socket.id] = name;
-    socket.emit("join", name + " has connected to the server.");
+  socket.on("input", function (input) {
+    switch (input) {
+      case 'ready':
+        ready('data');
+        break;
+    }
   });
 
-  socket.on("ready", function(){
-    people[socket.id].ready = true;
-    socket.emit("update", "You have connected to the server.");
+  socket.on("join", function (data) {
+    join(data);
   });
 
-  socket.on('countdown', function(msg){
-    io.emit('countdown', msg);
+  socket.on("ready", function (data) {
+    ready(data);
   });
 
-  socket.on('end', function(msg){
-    io.emit('countdown', msg);
+  socket.on("ready", function (data) {
+    ready(data);
   });
 
-  socket.on("send", function(msg){
-    //socket.sockets.emit("chat", people[client.id], msg);
-  });
-
-  socket.on("disconnect", function(){
-    //socket.sockets.emit("update", people[socket.id] + " has left the server.");
-    //delete people[socket.id];
-    //socket.sockets.emit("update-people", people);
+  socket.on('end', function (data) {
+    end(data);
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
+
+
+/***********************/
+/******* Methods *******/
+/***********************/
+
+function join(data) {
+  people[socket.id] = {
+    name: data,
+    ready: false
+  };
+  socket.emit("join", people[socket.id].name + " has connected to the server.");
+}
+
+function ready(data) {
+  people[socket.id].ready = true;
+  socket.emit("ready", people);
+  if (checkAllReady(people)) {
+    countdown();
+  }
+}
+
+function countdown() {
+  var iterations = 0, max = 3;
+  var interval = setInterval(function () {
+    iterations++;
+    io.emit('countdown', (max - iterations));
+    if (iterations === max) {
+      clearInterval(interval);
+    }
+  }, 1000);
+  io.emit('countdown', max);
+}
+
+function end(data) {
+
+}
+
+function checkAllReady(dict) {
+  var key, user;
+  for (var i = 0; i < dict.length; i++) {
+    key = dict.toArray[i];
+    user = dict[key];
+    if (!user.ready) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function Dictionary() {
+  return Object.create({}, {
+    length: {
+      get: function () {
+        return Object.keys(this).length;
+      }
+    },
+    toArray: {
+      get: function () {
+        return Object.keys(this);
+      }
+    }
+  });
+}
+
 
